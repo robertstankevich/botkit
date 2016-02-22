630d9112748d09f6af452b1029cae13ad4749911
thatsafy <antti.alasalmi@metropolia.fi>
2016-02-22 13:15:36 +0200

    added maker command

--- bot.js
+++ bot.js
@@ -83,6 +83,13 @@ var bot = controller.spawn({
 }).startRTM();
 
 
+controller.hears(['maker', 'who made you?',' who made you'], 'direct_message,direct_mention,mention',function(bot, message) {
+    controller.storage.users.get(message.user,function(err,user){
+        bot.reply(message,'Antti made me!');
+    }
+    });
+});
+
 controller.hears(['hello','hi'],'direct_message,direct_mention,mention',function(bot, message) {
 
     bot.api.reactions.add({
